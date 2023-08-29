7c037959b7379223bb3aa174092d0b2f3d6125ba
Ali Raza Farishta <yasirupworkdev@gmail.com>
2023-08-29 17:36:11 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'http://www.dentfolio.co.uk';
+  : 'https://www.dentfolio.co.uk';
 
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyDtNQLSo9z2j996yTIBxmxRTseja8eQhgo';
