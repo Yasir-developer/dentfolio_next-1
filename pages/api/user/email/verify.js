27c4983f22bbcbc3561bd75d52e808be0a833579
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-08-31 17:43:21 +0500

    update verify email template

--- pages/api/user/email/verify.js
+++ pages/api/user/email/verify.js
@@ -18,17 +18,81 @@ handler.post(async (req, res) => {
     type: 'emailVerify',
     expireAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
   });
+  const emailTemplate = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              margin: 0;
+              padding: 0;
+          }
+          .container {
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 20px;
+              background-color: #ffffff;
+          }
+          .header {
+<!--                 background-color: #007bff; -->
+              color: #ffffff;
+              padding: 10px;
+<!--           width:100px -->
+<!--                 text-align: center; -->
+          }
+          .content {
+              padding: 10px;
+          }
+        .headerText{
+        font-size:20px;
+<!--           font-style:bold -->
+        color:#000000
+        }
+        .dentText{
+        color:#0372E2
+        }
+        .heading{
+        color:#d6d3cc;
+         font-weight: bold;
+        }
+        .footer{
+          height:20px; background-color:#001323; width:600px; align-items:center; justify-content:center
+        }
+        .footerText{
+          text-align:center; color:#fff; font-size:14px; background-color:#001323        }
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <div class="header">
+          <img src="${server}/images/logo.png" alt="Sample Image" />
+        
+            <h1 class="headerText" style="font-size:20px; color:#000000">Hello 
+                 <span class="dentText" style="color:#0372E2">${firstName}</span></h1>
+          </div>
+          <div class="content">
+          <p>Please follow <a href="${server}/verify-email/${token._id}">this link</a> to confirm your email.</p>
+
+           
+          </div>
+
+          <footer style="height:20px; background-color:#001323; width:600px; align-items:center; justify-content:center">
+          <p  class="footerText" style="text-align:center; color:#fff; font-size:14px; background-color:#001323"  >
+                  © Copyright 2023 Dentfolio. All Rights Reserved.
+                  <p>
+            </footer>
+      </div>
+  </body>
+  </html>
+    `;
 
   await sendMail({
     to: email,
     from: 'contact@dentfolio.co.uk',
     subject: `Verification Email for ${server}`,
-    html: `
-      <div>
-        <p>Hello, ${firstName}</p>
-        <p>Please follow <a href="${server}/verify-email/${token._id}">this link</a> to confirm your email.</p>
-      </div>
-      `,
+    html: emailTemplate,
   });
 
   res.json({
