4eb3695392013281dce2b0009a062ca938f88c0e
Yaisr Iqbal <yasirupworkdev@gmail.com>
2023-09-21 01:43:33 +0500

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -27,6 +28,16 @@ class MyDocument extends Document {
           <script src="https://www.google.com/recaptcha/api.js"></script>
 
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-0BTT5R168E"></script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+
+
+              gtag('config', 'G-0BTT5R168E');
+            `}
+          </Script>
         </Head>
         <body className="h-full">
           <Main />
